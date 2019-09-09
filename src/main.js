e6c57b4309df308beb7c68a7c8e6eeab74e96cf7
hhxhls <hhxalex@163.com>
2019-09-09 09:52:47 +0800

    fix incorrect scroll behavior when router changes

--- src/main.js
+++ src/main.js
@@ -19,6 +19,12 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
   mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  },
 });
 
 router.beforeEach((to, from, next) => {
